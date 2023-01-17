cf9347fc71d510362352e6d0d0f61c207f92acc0
Alexander Pilz <alex.pilz95@googlemail.com>
2023-01-17 22:27:57 +0100

    Fixed portolan

--- portolan_mapper.py
+++ portolan_mapper.py
@@ -1,28 +1,25 @@
 import math
 import json
 
-def getPortolan(x, y, size, layout):
-    anglesFull = [360, 22.5, 45, 67.5, 180, 202.5, 225, 247.5, 90, 112.5, 135, 157.5, 270, 292.5, 315, 337.5]
+def getPortolan(bbox, layout):
+    anglesInner = [360, 22.5, 45, 67.5, 180, 202.5, 225, 247.5, 90, 112.5, 135, 157.5, 270, 292.5, 315, 337.5]
+    anglesOuter = [11.25, 22.5, 33.75, 45.0, 56.25, 67.5, 78.75, 90.0, 101.25, 112.5, 123.75, 135.0, 146.25, 157.5, 168.75, 180.0, 191.25, 202.5, 213.75, 225.0, 236.25, 247.5, 258.75, 270.0, 281.25, 292.5, 303.75, 315.0, 326.25, 337.5, 348.75, 360.0]
     anglesLeft = [360, 22.5, 45, 67.5, 90, 337.5, 315, 292.5, 270]
     anglesRight = [180, 270, 247.5, 225, 202.5, 157.5, 135, 112.5, 90]
     
-    if(layout == "oo"):
-        getGeometries(x+size, y, anglesFull, "left", size)
-        getGeometries(x-size, y, anglesFull, "right", size)  
-    elif(layout == '(o)'):
-        getGeometries(x, y, anglesFull, "center", size)
-        getGeometries(x+size, y, anglesLeft, "left", size)
-        getGeometries(x-size, y, anglesRight, "right", size)
-    elif(layout == ')o('):
-        getGeometries(x, y, anglesFull, "center", size)
-        getGeometries(x+2*size, y, anglesRight, "left", size)
-        getGeometries(x-2*size, y, anglesLeft, "right", size)
+
+        
     
-def getConnections(pointsCenter, pointsSide):
-    pass
+    xCenter = abs(bbox[0][0]) - abs(bbox[1][0])
+    yCenter = abs(bbox[0][1]) - abs(bbox[1][1])
     
-def getGeometries(x, y, angles, name, size):
+    sizeX = abs(bbox[0][0])
+    sizeY = abs(bbox[0][1])
     
+    if(layout == 'o'):
+        getGeometries(xCenter, yCenter, anglesInner, anglesOuter, "center", sizeX, sizeY)
+        
+def getGeometries(x, y, anglesInner, anglesOuter, name, sizeX, sizeY):    
     point = {
       "type": "Feature",
       "properties": {"angle": 0},
@@ -37,9 +34,9 @@ def getGeometries(x, y, angles, name, size):
     points = []
     points.append(point)
     
-    for i in angles:
-        px = x + (size * math.cos(i*(math.pi/180)))  
-        py = y + (size * math.sin(i*(math.pi/180)))
+    for i in anglesInner:
+        px = x + (sizeX * math.cos(i*(math.pi/180)))  
+        py = y + (sizeY * math.sin(i*(math.pi/180)))
         
         
         point = {
@@ -63,9 +60,10 @@ def getGeometries(x, y, angles, name, size):
     out_file = open(name + "pointsData.json", "w")
     json.dump(pointsData, out_file, indent = 6)
     out_file.close()
-    
+
     lines = []
     id = 0
+    '''
     for i in points:
         for y in points:
             line = {
@@ -87,6 +85,34 @@ def getGeometries(x, y, angles, name, size):
            }
             lines.append(line)
             id += 1
+    '''
+    for p in points:
+        
+        x1 = p["geometry"]["coordinates"][0]
+        y1 = p["geometry"]["coordinates"][1]
+        for a in anglesOuter:
+            x2 = x1 + (180 * math.cos(a*(math.pi/180)))  
+            y2 = y1 + (180 * math.sin(a*(math.pi/180)))
+            line = {
+             "type": "Feature",
+             "properties": {'id': id, 'angle': a},
+             "geometry": {
+               "coordinates": [
+                 [
+                   x1,
+                   y1
+                 ],
+                 [
+                   x2,
+                   y2
+                 ]
+               ],
+               "type": "LineString"
+             }
+           }
+            lines.append(line)
+            id += 1
+        
             
     linesData = {
           "type": "FeatureCollection",
@@ -99,9 +125,5 @@ def getGeometries(x, y, angles, name, size):
         
     return True
 
-#Types: oo, (o), )o(
-#getPortolan(0, 0, 90, 'oo')
-#getPortolan(0, 0, 90, '(o)')
-getPortolan(0, 0, 90, ')o(')
-
+getPortolan([(-90, 90), (90, -90)], 'o')
 
